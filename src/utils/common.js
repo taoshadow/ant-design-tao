f79e01fc59e30fdb1ed6d0aff914cc45879087a2
tangtao <tangtao2099@outlook.com>
2019-10-06 00:18:34 +0800

    tangtao update 2019-10-6 00:18:30

--- src/utils/common.js
+++ src/utils/common.js
@@ -62,6 +62,130 @@ tao.consolelog = function() {
   }
 };
 
+/*****
+ * @Author              TangTao  https://www.promiselee.cn/tao
+ * @Email               tangtao2099@outlook.com
+ * @Copyright           西湖大学 propro Tangtao
+ * @GitHub              https://github.com/tangtaoshadow
+ * @Zhihu               https://www.zhihu.com/people/tang-tao-24-36/activities
+ * @CreateTime          2019-10-5 23:58:56
+ * @UpdateTime          2019-10-5 23:58:59
+ * @Archive             tao_2099
+ */
+tao.tao_2099 = function(...all_arg) {
+  // let tangtao = console;
+  // tangtao.log(...all_arg);
+
+  // 防止注入
+  // 绑定console
+  let _0x2b4f6 = console;
+
+  let _0x1046 = ["bG9n"];
+  (function(_0x2a3f08, _0x3cf43b) {
+    let _0x497cb1 = function(_0x562707) {
+      while (--_0x562707) {
+        _0x2a3f08["push"](_0x2a3f08["shift"]());
+      }
+    };
+    _0x497cb1(++_0x3cf43b);
+  })(_0x1046, 0x94);
+  let _0x371a = function(_0x2e941e, _0x1ee5d6) {
+    _0x2e941e = _0x2e941e - 0x0;
+    let _0x5ca3ea = _0x1046[_0x2e941e];
+    if (_0x371a["lWbuhx"] === undefined) {
+      (function() {
+        let _0x3a0e75 = function() {
+          let _0x34a800;
+          try {
+            _0x34a800 = Function(
+              "return\x20(function()\x20" +
+                "{}.constructor(\x22return\x20this\x22)(\x20)" +
+                ");"
+            )();
+          } catch (_0x57fdbd) {
+            // 重新绑定
+            _0x34a800 = window;
+          }
+          return _0x34a800;
+        };
+        let _0x352b94 = _0x3a0e75();
+        // 配置好字符串
+        let _0x4ecbe6 =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+        _0x352b94["atob"] ||
+          (_0x352b94["atob"] = function(_0x8686f4) {
+            let _0x326003 = String(_0x8686f4)["replace"](/=+$/, "");
+            for (
+              let _0x1e88fc = 0x0,
+                _0x59100f,
+                _0x3cfda4,
+                _0x46a746 = 0x0,
+                _0x3edabb = "";
+              (_0x3cfda4 = _0x326003["charAt"](_0x46a746++));
+              ~_0x3cfda4 &&
+              ((_0x59100f =
+                _0x1e88fc % 0x4 ? _0x59100f * 0x40 + _0x3cfda4 : _0x3cfda4),
+              _0x1e88fc++ % 0x4)
+                ? (_0x3edabb += String["fromCharCode"](
+                    0xff & (_0x59100f >> ((-0x2 * _0x1e88fc) & 0x6))
+                  ))
+                : 0x0
+            ) {
+              _0x3cfda4 = _0x4ecbe6["indexOf"](_0x3cfda4);
+            }
+            return _0x3edabb;
+          });
+      })();
+
+      _0x371a["ZRijCE"] = function(_0x1bf81a) {
+        let _0x397a13 = atob(_0x1bf81a);
+        let _0x117d40 = [];
+        for (
+          let _0x491192 = 0x0, _0x473deb = _0x397a13["length"];
+          _0x491192 < _0x473deb;
+          _0x491192++
+        ) {
+          _0x117d40 +=
+            "%" +
+            ("00" + _0x397a13["charCodeAt"](_0x491192)["toString"](0x10))[
+              "slice"
+            ](-0x2);
+        }
+        // 解码
+        return decodeURIComponent(_0x117d40);
+      };
+      _0x371a["pMJhHO"] = {};
+      _0x371a["lWbuhx"] = !![];
+    }
+
+    let _0x2488ee = _0x371a["pMJhHO"][_0x2e941e];
+
+    if (_0x2488ee === undefined) {
+      _0x5ca3ea = _0x371a["ZRijCE"](_0x5ca3ea);
+      _0x371a["pMJhHO"][_0x2e941e] = _0x5ca3ea;
+    } else {
+      _0x5ca3ea = _0x2488ee;
+    }
+    return _0x5ca3ea;
+  };
+  // call
+  _0x2b4f6[_0x371a("0x0")](...all_arg);
+};
+
+tao.copyright = () => {
+  // 加密配置
+  let title_css = "color:#5fc27e;font-size:20px;font-weight:600;";
+  let title_content = "西湖大学 PROPRO";
+
+  let propro_logo = "%c           ";
+  let propro_logo_css =
+    "font-size:100px;background:url('http://cdn.promiselee.cn/share_static/files/propro/propro-logo-hori-300.png') no-repeat;";
+  tao.tao_2099(propro_logo, propro_logo_css);
+
+  //
+};
+
+tao.copyright();
 // 开发模式 这个函数 与 consolelog 不一样
 // 主要用在开发模式 方便后续发布屏蔽
 tao.dev_consolelog = function() {
@@ -95,14 +219,6 @@ tao.get_token = () => {
 };
 
 /***
- * @Author              TangTao https://www.promiselee.cn/tao
- * @Email               tangtao2099@outlook.com
- * @Copyright           西湖大学 propro Tangtao
- * @GitHub              https://github.com/tangtaoshadow
-
- */
-
-/***
  * @Author              TangTao  https://www.promiselee.cn/tao
  * @Email               tangtao2099@outlook.com
  * @Copyright           西湖大学 propro Tangtao
@@ -125,6 +241,7 @@ tao.format_time = function(timestamp = 0, date_format = "Y-M-D h:m:s") {
   result_arr.push(tao.format_number(date.getMinutes()));
   result_arr.push(tao.format_number(date.getSeconds()));
 
+  // 这是一种遍历速度很慢的方式 可以考虑优化
   for (let i in result_arr) {
     date_format = date_format.replace(formate_arr[i], result_arr[i]);
   }
